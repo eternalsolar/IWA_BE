3ed3e78910d6b9eeccee6afe4bc2931944a3d24e
Eternal Solar <eternalsolar0106@gmail.com>
2020-11-09 15:57:54 +0700

    fix

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,13 @@ const client = redis.createClient({
     password: '1234@Qwe',
 })
 let a: Article[] = [];
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.get('/article/:link', (req, res) => {
     let Crawler = require('crawler');
     let extractor = require('unfluff');
